31203f446cc914056d76d1425d096bffd75da5fa
Leonardo Lopes <Leolopees@yahoo.com.br>
2025-02-28 16:53:00 -0300

    Exercícios: 1 e 2 ✔️

---
+++ desenvolvimento-csharp/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua data de nascimento dd/mm/yyyy: ");
+            string dataNascimento = Console.ReadLine();
+            
+            try
+            {
+                DateTime dataFormatada = DateTime.ParseExact(dataNascimento, "dd/MM/yyyy", null, DateTimeStyles.None);
+                DateTime dataAtual = DateTime.Now;
+                
+                int anos = dataAtual.Year - dataFormatada.Year;
+                int meses = dataAtual.Month - dataFormatada.Month;
+                int dias = dataAtual.Day - dataFormatada.Day;
+
+                if (dias < 0)
+                {
+                    meses--;
+                    DateTime mesAnterior = dataAtual.AddMonths(-1);
+                    dias += DateTime.DaysInMonth(mesAnterior.Year, mesAnterior.Month); // ano bissexto
+                }
+
+                if (meses < 0)
+                {
+                    anos--;
+                    meses += 12;
+                }
+                
+                Console.WriteLine($"Você tem {anos} anos, {meses} meses e {dias} dias.");
+            } catch
+            {
+                Console.WriteLine("Data inválida.");
+            }
+        }
+    }
+}
+
+
---
+++ desenvolvimento-csharp/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Data de nascimento dd/mm/yyyy: ");
+        try
+        {
+            string dataNascimento = Console.ReadLine();
+            DateTime dataFormatada = DateTime.ParseExact(dataNascimento, "dd/MM/yyyy", null);
+            DateTime hoje = DateTime.Now;
+            DateTime aniversarioEsteAno = new DateTime(hoje.Year, dataFormatada.Month, dataFormatada.Day);
+
+            if (aniversarioEsteAno < hoje)
+            {
+                aniversarioEsteAno = new DateTime(hoje.Year + 1, dataFormatada.Month, dataFormatada.Day);
+            }
+            int diasRestantes = (aniversarioEsteAno - hoje).Days + 1;
+            Console.WriteLine($"Faltam {diasRestantes} dias para o seu aniversário!");
+        }
+        catch
+        {
+            Console.WriteLine("Data inválida.");
+        }
+    }
+}
