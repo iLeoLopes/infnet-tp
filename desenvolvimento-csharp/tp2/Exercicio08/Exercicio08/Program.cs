8f8da233276c04728d2f1cf17e4ff98cdf4076fd
Leonardo Lopes <Leolopees@yahoo.com.br>
2025-03-03 15:39:14 -0300

    Exercício: 08 ✔️

---
+++ desenvolvimento-csharp/tp2/Exercicio08/Exercicio08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Exercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota do aluno: ");
+            double nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (nota < 7.0)
+            {
+                Console.WriteLine("Nota insuficiente.");
+            }
+            else if (nota < 8.0)
+            {
+                Console.WriteLine("Nota Regular.");
+            }
+            else if (nota < 9.0)
+            {
+                Console.WriteLine("Nota boa!");
+            }
+            else
+            {
+                Console.WriteLine("Nota EXCELENTE!!");
+            }
+        }
+    }
+}
