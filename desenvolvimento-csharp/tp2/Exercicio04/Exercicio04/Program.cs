cc65d8864308f6fb8f4350c9c1a68e936fb77db0
Leonardo Lopes <Leolopees@yahoo.com.br>
2025-03-03 14:42:19 -0300

    Exercício: 04 ✔️

---
+++ desenvolvimento-csharp/tp2/Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu telefone: ");
+            string telefone =Console.ReadLine();
+
+            Console.Write("Digite seu e-mail: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine($"\nUsuário: \n{nome}, {idade} anos.\nEmail: {email} \nTelefone: {telefone}");
+        }
+    }
+}
