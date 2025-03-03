f1267b1903fdf55ac4fca781750164d7af2c012b
Leonardo Lopes <Leolopees@yahoo.com.br>
2025-03-03 16:37:41 -0300

    Exercício: 11 ✔️

---
+++ desenvolvimento-csharp/tp2/Exercicio11/Exercicio11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para gerar a tabuada de 1 a 10: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {i * numero}");
+            }
+        }
+    }
+}
