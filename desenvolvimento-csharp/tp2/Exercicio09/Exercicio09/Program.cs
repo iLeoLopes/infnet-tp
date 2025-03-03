ed4cc31ff68f4ee06361a8519d63179d7d1aab75
Leonardo Lopes <Leolopees@yahoo.com.br>
2025-03-03 16:22:41 -0300

    Exercício: 09 ✔️

---
+++ desenvolvimento-csharp/tp2/Exercicio09/Exercicio09/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace Exercicio09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu salário bruto: ");
+            double salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salarioLiquido = salarioBruto;
+            double imposto;
+            double descontado;
+
+            if (salarioBruto >= 4664)
+            {
+                imposto = 0.275;
+                salarioLiquido -= (salarioBruto * imposto);
+                descontado = salarioBruto * imposto;
+            }
+            else if (salarioBruto >= 3752)
+            {
+                imposto = 0.225;
+                salarioLiquido -= (salarioBruto * imposto);
+                descontado = salarioBruto * imposto;
+
+            }
+            else if (salarioBruto >= 2827)
+            {
+                imposto = 0.15;
+                salarioLiquido -= (salarioBruto * imposto);
+                descontado = salarioBruto * imposto;
+
+            }
+            else if (salarioBruto >= 2259)
+            {
+                imposto = 0.075;
+                salarioLiquido -= (salarioBruto * imposto);
+                descontado = salarioBruto * imposto;
+            }
+            else
+            {
+                imposto = 0.0;
+                salarioLiquido -= imposto;
+                descontado = salarioBruto * imposto;
+            }
+            Console.WriteLine($"\nSalarioBruto: R$ {salarioBruto.ToString("F2", CultureInfo.InvariantCulture)}" +
+                    $"\nTotal descontado: R$ {descontado.ToString("F2", CultureInfo.InvariantCulture)}" +
+                    $"\nSalário Liquido: R$ {salarioLiquido.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
