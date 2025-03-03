14ecf7f9b4278e4241a258c137c0262328d4d401
Leonardo Lopes <Leolopees@yahoo.com.br>
2025-03-03 15:22:02 -0300

    Exercício: 07 ✔️

---
+++ desenvolvimento-csharp/tp2/Exercicio07/Exercicio07/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            string resutado = numero % 2 == 0 ? "par" : "ímpar";
+            Console.WriteLine($"O número {numero} é {resutado}.");
+        }
+    }
+}
