b4f4e3179aa3910f42170728b936d6a51d909308
Leonardo Lopes <Leolopees@yahoo.com.br>
2025-03-03 14:54:37 -0300

    Exercício: 05 ✔️

---
+++ desenvolvimento-csharp/tp2/Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor em graus Celsius para conversão: ");
+            double celsius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double fharenheit = (celsius * 9 / 5) + 32;
+            double kelvin = celsius + 273.15;
+
+            Console.WriteLine(
+                $"Fahrenheit: {fharenheit.ToString("F2", CultureInfo.InvariantCulture)}°F" +
+                $"\nKelvin: {kelvin.ToString("F2", CultureInfo.InvariantCulture)} K");
+        }
+    }
+}
