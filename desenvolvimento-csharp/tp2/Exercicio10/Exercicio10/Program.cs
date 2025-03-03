da8e3f67a90e45de48385e6aeaa79daaaed0efef
Leonardo Lopes <Leolopees@yahoo.com.br>
2025-03-03 16:33:24 -0300

    Exercício: 10 ✔️

---
+++ desenvolvimento-csharp/tp2/Exercicio10/Exercicio10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para iniciar a contagem regressiva: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            while(numero > 0)
+            {
+                Console.Write($"{numero}, ");
+                numero--;
+            }
+            Console.WriteLine(0);
+        }
+    }
+}
